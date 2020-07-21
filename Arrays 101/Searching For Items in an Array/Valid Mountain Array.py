2ba85625c5a5dd5ef41dd7548f941253f4a25270
Thedla Eshwar Chandra VidhyaSagar <thedla.ecvs@gmail.com>
2020-07-22 00:09:36 +0530

    Create Valid Mountain Array.py

---
+++ Arrays 101/Searching For Items in an Array/Valid Mountain Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validMountainArray(self, A):
+        i = 0
+        while i+1 < len(A) and A[i] < A[i+1]:
+            i += 1
+        if i == 0 or i == len(A)-1:
+            return False
+        while i+1 < len(A) and A[i] > A[i+1]:
+            i += 1
+        return i == len(A)-1
