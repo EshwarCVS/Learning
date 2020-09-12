7f5d5c34a0aba389158d9e12bc7af0c7aff93147
Thedla Eshwar Chandra Vidhyasagar <thedla.ecvs@gmail.com>
2020-09-12 23:38:39 +0530

    Create sortedSquares.cpp

---
+++ Leetcode/Arrays 101/In-Place Operations/sortedSquares.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for(auto it = A.begin();it < A.end(); ++it){
+            *it *= *it; 
+        }
+        sort(A.begin(),A.end());
+        return A;
+    }
+};
