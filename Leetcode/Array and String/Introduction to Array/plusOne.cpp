f8a382f3d847cff431f43faf4b60dee61573a9ef
Thedla Eshwar Chandra Vidhyasagar <thedla.ecvs@gmail.com>
2020-09-13 10:19:03 +0530

    Create plusOne.cpp

---
+++ Leetcode/Array and String/Introduction to Array/plusOne.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& arr) {
+        int size = arr.size();
+        for (int i = size - 1; i >= 0; --i) {
+            if (arr[i] == 9) {
+                arr[i] = 0;
+            } else {
+                arr[i] = arr[i] + 1;
+                break;
+            }
+        }
+
+        if (!arr[0]) {
+            arr.resize(++size);
+            arr[0] = 1;
+        }
+        return arr;
+    }
+};
