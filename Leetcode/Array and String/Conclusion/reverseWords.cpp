bc731669fce58aedb9001940ddecf8d392411665
Thedla Eshwar Chandra Vidhyasagar <thedla.ecvs@gmail.com>
2020-09-14 20:18:49 +0530

    Update reverseWords.cpp

--- Leetcode/Array and String/Conclusion/reverseWords.cpp
+++ Leetcode/Array and String/Conclusion/reverseWords.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1165/
+// using swap
 class Solution {
 public:
     string reverseWords(string s) {
@@ -14,6 +16,23 @@ public:
           i++; end--;
         }
         i = j + 2;
+      }
+      return s;
+    }
+};
+
+// using reverse
+class Solution {
+public:
+    string reverseWords(string s) {
+      if(s.size() < 2){return s;}
+      int i=0, j;
+      while(i< s.size()){
+        j =i + 1;
+        while(s[j] != ' ' && j < s.size())
+          j++;
+        reverse(begin(s)+i, begin(s)+j);
+        i = j + 1;
       }
       return s;
     }
